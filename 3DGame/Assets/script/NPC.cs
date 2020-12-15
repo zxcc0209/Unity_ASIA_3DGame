9f080de5cb94d49aca569c8dbae68e95ea3bd3b2
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-12-15 15:22:03 +0800

    對話

---
+++ 3DGame/Assets/script/NPC.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData data;
+
+    public bool PlayerinArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            PlayerinArea = true;
+            Dialoug();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            PlayerinArea = false;
+        }
+    }
+    private void Dialoug()
+    {
+        print(data.dialougA);
+    }
+}
---
+++ 3DGame/Assets/script/NPCData.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+[CreateAssetMenu(fileName ="NPC資料",menuName ="Stanley/NPC資料")]
+public class NPCData : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1, 5)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已經取得項目數量")]
+    public int countCurrent;
+}
